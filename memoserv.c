24810b644dc532d831a5e1f2fa8d17d4b562ce77
certus certus@31f1291d-b8d6-0310-a050-a5561fc1590b <certus certus@31f1291d-b8d6-0310-a050-a5561fc1590b@5417fbe8-f217-4b02-8779-1006273d7864>
2004-06-04 14:17:43 +0000

    BUILD : 1.7.3 (167) BUGS : NOTES : Fixed a typo in my last submit, which caused a compile error.
    git-svn-id: svn://svn.anope.org/anope/trunk@167 31f1291d-b8d6-0310-a050-a5561fc1590b
    
    
    git-svn-id: http://anope.svn.sourceforge.net/svnroot/anope/trunk@116 5417fbe8-f217-4b02-8779-1006273d7864

--- memoserv.c
+++ memoserv.c
@@ -305,7 +305,7 @@ void memo_send(User * u, char *name, char *text, int z)
             notice_lang(s_MemoServ, u, MEMO_X_GETS_NO_MEMOS, name);
 
     } else if (mi->memomax > 0 && mi->memocount >= mi->memomax
-               && !is_servop) {
+               && !is_servoper) {
         if (z == 0 || z == 3)
             notice_lang(s_MemoServ, u, MEMO_X_HAS_TOO_MANY_MEMOS, name);
 

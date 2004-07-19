faf3709fd8cabfb50bf2e54c4d24cd1e2fed7b8d
rob rob@31f1291d-b8d6-0310-a050-a5561fc1590b <rob rob@31f1291d-b8d6-0310-a050-a5561fc1590b@5417fbe8-f217-4b02-8779-1006273d7864>
2004-07-19 14:11:57 +0000

    BUILD : 1.7.4 (265) BUGS : N/A NOTES : Only forgot 2 changes :)
    git-svn-id: svn://svn.anope.org/anope/trunk@265 31f1291d-b8d6-0310-a050-a5561fc1590b
    
    
    git-svn-id: http://anope.svn.sourceforge.net/svnroot/anope/trunk@170 5417fbe8-f217-4b02-8779-1006273d7864

--- src/modules/module.h
+++ src/modules/module.h
@@ -1,7 +1,7 @@
-#include "../services.h"
-#include "../commands.h"
-#include "../language.h"
-#include "../modules.h"
+#include "services.h"
+#include "commands.h"
+#include "language.h"
+#include "modules.h"
 
 #define MOD_UNIQUE 0
 #define MOD_HEAD   1

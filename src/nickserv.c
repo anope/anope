474ee0c00a45abef28361e7c424102dd07667b6a
rob rob@31f1291d-b8d6-0310-a050-a5561fc1590b <rob rob@31f1291d-b8d6-0310-a050-a5561fc1590b@5417fbe8-f217-4b02-8779-1006273d7864>
2004-09-21 16:02:58 +0000

    BUILD : 1.7.5 (355) BUGS : N/A NOTES : An option to not detect mysql has been added to ./Config
    git-svn-id: svn://svn.anope.org/anope/trunk@355 31f1291d-b8d6-0310-a050-a5561fc1590b
    
    
    git-svn-id: http://anope.svn.sourceforge.net/svnroot/anope/trunk@231 5417fbe8-f217-4b02-8779-1006273d7864

--- src/nickserv.c
+++ src/nickserv.c
@@ -1980,19 +1980,22 @@ static int do_register(User * u)
     if (RestrictOperNicks) {
         for (i = 0; i < RootNumber; i++) {
             if (strstr(u->nick, ServicesRoots[i]) && !is_oper(u)) {
-                notice_lang(s_NickServ, u, NICK_CANNOT_BE_REGISTERED, u->nick);
+                notice_lang(s_NickServ, u, NICK_CANNOT_BE_REGISTERED,
+                            u->nick);
                 return MOD_CONT;
             }
         }
-        for (i = 0; i < servadmins.count && (nc = servadmins.list[i]);i++) {
+        for (i = 0; i < servadmins.count && (nc = servadmins.list[i]); i++) {
             if (strstr(u->nick, nc->display) && !is_oper(u)) {
-                notice_lang(s_NickServ, u, NICK_CANNOT_BE_REGISTERED, u->nick);
+                notice_lang(s_NickServ, u, NICK_CANNOT_BE_REGISTERED,
+                            u->nick);
                 return MOD_CONT;
             }
         }
-        for (i = 0; i < servopers.count && (nc = servopers.list[i]);i++) {
+        for (i = 0; i < servopers.count && (nc = servopers.list[i]); i++) {
             if (strstr(u->nick, nc->display) && !is_oper(u)) {
-                notice_lang(s_NickServ, u, NICK_CANNOT_BE_REGISTERED, u->nick);
+                notice_lang(s_NickServ, u, NICK_CANNOT_BE_REGISTERED,
+                            u->nick);
                 return MOD_CONT;
             }
         }

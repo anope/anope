anope/anope

commit 85b409df8577e2433724a056564ab62ff72c4c89

Removed NS_IDENTIFIED/NS_RECOGNIZED/NS_ON_ACCESS from na->status, it doesnt belong there anymore and doesnt work. Replaced nick_recognized() with User::IsRecognized() which returns if youre recognized for the nick you are on, not necessarially the account youre logged in for.
git-svn-id: http://anope.svn.sourceforge.net/svnroot/anope/trunk@2523 5417fbe8-f217-4b02-8779-1006273d7864

src/nickserv.c
@@ -56,9 +56,10 @@ class NickServCollide : public Timer
 	void Tick(time_t ctime)
 	{
 		/* If they identified or don't exist anymore, don't kill them. */
-		if ((na->status & NS_IDENTIFIED) || !finduser(na->nick)
-				|| finduser(na->nick)->my_signon > this->GetSetTime())
+		User *u = finduser(na->nick);
+		if (!u || !nick_identified(u) || u->my_signon > this->GetSetTime())
 			return;
+
 		/* The RELEASE timeout will always add to the beginning of the
 		 * list, so we won't see it.  Which is fine because it can't be
 		 * triggered yet anyway. */
@@ -622,8 +623,6 @@ int validate_user(User * u)
 	NickRequest *nr;
 	NickServCollide *t;
 
-	int on_access;
-
 	if ((nr = findrequestnick(u->nick)))
 	{
 		notice_lang(s_NickServ, u, NICK_IS_PREREG);
@@ -646,13 +645,8 @@ int validate_user(User * u)
 		return 0;
 	}
 
-	on_access = is_on_access(u, na->nc);
-	if (on_access)
-		na->status |= NS_ON_ACCESS;
-
-	if (!(na->nc->flags & NI_SECURE) && on_access)
+	if (!(na->nc->flags & NI_SECURE) && u->IsRecognized())
 	{
-		na->status |= NS_RECOGNIZED;
 		na->last_seen = time(NULL);
 		if (na->last_usermask)
 			delete [] na->last_usermask;
@@ -665,7 +659,7 @@ int validate_user(User * u)
 		return 1;
 	}
 
-	if (on_access || !(na->nc->flags & NI_KILL_IMMED))
+	if (u->IsRecognized() || !(na->nc->flags & NI_KILL_IMMED))
 	{
 		if (na->nc->flags & NI_SECURE)
 			notice_lang(s_NickServ, u, NICK_IS_SECURE, s_NickServ);
@@ -673,7 +667,7 @@ int validate_user(User * u)
 			notice_lang(s_NickServ, u, NICK_IS_REGISTERED, s_NickServ);
 	}
 
-	if ((na->nc->flags & NI_KILLPROTECT) && !on_access)
+	if ((na->nc->flags & NI_KILLPROTECT) && !u->IsRecognized())
 	{
 		if (na->nc->flags & NI_KILL_IMMED)
 		{
@@ -753,34 +747,6 @@ int nick_identified(User * u)
 
 /*************************************************************************/
 
-/* Return whether a user is recognized for their nickname. */
-
-int nick_recognized(User * u)
-{
-	if (u)
-	{
-		NickAlias *na = findnick(u->nick);
-		if (na)
-		{
-			if (na->status)
-			{
-				return (na->status & (NS_IDENTIFIED | NS_RECOGNIZED));
-			}
-			else
-			{
-				return 0;
-			}
-		}
-		else
-		{
-			return 0;
-		}
-	}
-	return 0;
-}
-
-/*************************************************************************/
-
 /* Remove all nicks which have expired.  Also update last-seen time for all
  * nicks.
  */
@@ -799,8 +765,7 @@ void expire_nicks()
 			next = na->next;
 
 			User *u = finduser(na->nick);
-			if (u
-			        && ((na->nc->flags & NI_SECURE) ? nick_identified(u) : nick_recognized(u)))
+			if (u && ((na->nc->flags & NI_SECURE) ? nick_identified(u) : u->IsRecognized()))
 			{
 				if (debug >= 2)
 					alog("debug: NickServ: updating last seen time for %s",
@@ -941,18 +906,20 @@ NickCore *findcore(const char *nick)
 /*********************** NickServ private routines ***********************/
 /*************************************************************************/
 
-/* Is the given user's address on the given nick's access list?  Return 1
- * if so, 0 if not. */
-
-int is_on_access(User * u, NickCore * nc)
+/** Is the user's address on the nickcores access list?
+ * @param u The user
+ * @param nc The nickcore
+ * @return true or false
+ */
+bool is_on_access(User *u, NickCore *nc)
 {
 	unsigned i;
 	char *buf;
 	char *buf2 = NULL;
 	char *buf3 = NULL;
 
-	if (nc->access.empty())
-		return 0;
+	if (!u || !nc || nc->access.empty())
+		return false;
 
 	buf = new char[u->GetIdent().length() + strlen(u->host) + 2];
 	sprintf(buf, "%s@%s", u->GetIdent().c_str(), u->host);
@@ -987,7 +954,7 @@ int is_on_access(User * u, NickCore * nc)
 					delete [] buf3;
 				}
 			}
-			return 1;
+			return true;
 		}
 	}
 	delete [] buf;
@@ -996,7 +963,7 @@ int is_on_access(User * u, NickCore * nc)
 		delete [] buf2;
 		delete [] buf3;
 	}
-	return 0;
+	return false;
 }
 
 /*************************************************************************/

include/extern.h
@@ -716,7 +716,6 @@ E void save_ns_req_dbase();
 E int validate_user(User * u);
 E void cancel_user(User * u);
 E int nick_identified(User * u);
-E int nick_recognized(User * u);
 E void expire_nicks();
 E void expire_requests();
 EI int ns_do_register(User * u);
@@ -724,7 +723,7 @@ E int delnick(NickAlias * na);
 E NickAlias *findnick(const char *nick);
 E NickAlias *findnick(const std::string &nick);
 E NickCore  *findcore(const char *nick);
-E int is_on_access(User * u, NickCore * nc);
+E bool is_on_access(User *u, NickCore *nc);
 
 /** Set the correct oper type for a nickcore
  * @param nc The nick core
@@ -904,8 +903,6 @@ E User *next_uid();
 E Server *findserver_uid(Server * s, const char *name);
 E char *TS6SID;
 E char *TS6UPLINK;
-
-E void update_host(User * user);
 
 E User *do_nick(const char *source, const char *nick, const char *username, const char *host,
 			  const char *server, const char *realname, time_t ts, uint32 ip, const char *vhost, const char *uid);

include/services.h
@@ -1102,9 +1102,9 @@ typedef struct ircd_modes_ {
 /* Nickname status flags: */
 #define NS_FORBIDDEN	0x0002	  /* Nick may not be registered or used */
 #define NS_NO_EXPIRE	0x0004	  /* Nick never expires */
-#define NS_IDENTIFIED	0x8000	  /* User has IDENTIFY'd */
-#define NS_RECOGNIZED	0x4000	  /* ON_ACCESS true && SECURE flag not set */
-#define NS_ON_ACCESS	0x2000	  /* User comes from a known address */
+//#define NS_IDENTIFIED	0x8000	  /* User has IDENTIFY'd */
+//#define NS_RECOGNIZED	0x4000	  /* ON_ACCESS true && SECURE flag not set */
+//#define NS_ON_ACCESS	0x2000	  /* User comes from a known address */
 #define NS_KILL_HELD	0x1000	  /* Nick is being held after a kill */
 #define NS_GUESTED	0x0100		/* SVSNICK has been sent but nick has not
 					 				 * yet changed. An enforcer will be

include/users.h
@@ -26,7 +26,9 @@ class CoreExport User : public Extensible
 	std::string vident;
 	std::string ident;
 	std::string uid;
-public: // XXX: exposing a tiny bit too much
+	bool OnAccess;	/* If the user is on the access list of the nick theyre on */
+
+ public: // XXX: exposing a tiny bit too much
 	User *next, *prev;
 
 	char nick[NICKMAX];
@@ -150,5 +152,14 @@ public: // XXX: exposing a tiny bit too much
 	 * @param account Display nick of account
 	 */
 	void AutoID(const char *acc);
+
+	/** Check if the user is recognized for their nick (on the nicks access list)
+	 * @return true or false
+	 */
+	const bool IsRecognized() const;
+
+	/** Update the last usermask stored for a user, and check to see if they are recognized
+	 */
+	void UpdateHost();
 };
 

src/channels.c
@@ -500,8 +500,7 @@ User *nc_on_chan(Channel * c, NickCore * nc)
 		return NULL;
 
 	for (u = c->users; u; u = u->next) {
-		if (u->user->nc == nc
-			&& nick_recognized(u->user))
+		if (u->user->nc == nc)
 			return u->user;
 	}
 	return NULL;

src/chanserv.c
@@ -1169,7 +1169,7 @@ int check_kick(User * user, const char *chan, time_t chants)
 		goto kick;
 	}
 
-	if (nick_recognized(user))
+	if (user->IsRecognized())
 		nc = user->nc;
 	else
 		nc = NULL;
@@ -1869,7 +1869,7 @@ int is_founder(User * user, ChannelInfo * ci)
 
 	if (user->nc && user->nc == ci->founder) {
 		if ((nick_identified(user)
-			 || (nick_recognized(user) && !(ci->flags & CI_SECURE))))
+			 || (user->IsRecognized() && !(ci->flags & CI_SECURE))))
 			return 1;
 	}
 	if (is_identified(user, ci))
@@ -1887,7 +1887,7 @@ int is_real_founder(User * user, ChannelInfo * ci)
 
 	if (user->nc && user->nc == ci->founder) {
 		if ((nick_identified(user)
-			 || (nick_recognized(user) && !(ci->flags & CI_SECURE))))
+			 || (user->IsRecognized() && !(ci->flags & CI_SECURE))))
 			return 1;
 	}
 	return 0;
@@ -1932,7 +1932,7 @@ int get_access(User * user, ChannelInfo * ci)
 		return 0;
 
 	if (nick_identified(user)
-		|| (nick_recognized(user) && !(ci->flags & CI_SECURE)))
+		|| (user->IsRecognized() && !(ci->flags & CI_SECURE)))
 		if ((access = ci->GetAccess(user->nc)))
 			return access->level;
 
@@ -1952,7 +1952,7 @@ void update_cs_lastseen(User * user, ChannelInfo * ci)
 		return;
 
 	if (is_founder(user, ci) || nick_identified(user)
-		|| (nick_recognized(user) && !(ci->flags & CI_SECURE)))
+		|| (user->IsRecognized() && !(ci->flags & CI_SECURE)))
 		if ((access = ci->GetAccess(user->nc)))
 			access->last_seen = time(NULL);
 }

src/core/ms_cancel.c
@@ -31,7 +31,7 @@ class CommandMSCancel : public Command
 		const char *name = params[0].c_str();
 		MemoInfo *mi;
 
-		if (!nick_recognized(u))
+		if (!u->IsRecognized())
 			notice_lang(s_MemoServ, u, NICK_IDENTIFY_REQUIRED, s_NickServ);
 		else if (!(mi = getmemoinfo(name, &ischan, &isforbid)))
 		{

src/core/ms_check.c
@@ -31,7 +31,7 @@ class CommandMSCheck : public Command
 		struct tm *tm;
 		char timebuf[64];
 
-		if (!nick_recognized(u))
+		if (!u->IsRecognized())
 		{
 			notice_lang(s_MemoServ, u, NICK_IDENTIFY_REQUIRED, s_NickServ);
 			return MOD_CONT;

commit 651aacd3e9f625871981924556639f1cecdc85af

Fixed a typo introduced in r2523 causing NickServ kill to not work
git-svn-id: http://anope.svn.sourceforge.net/svnroot/anope/trunk@2527 5417fbe8-f217-4b02-8779-1006273d7864

src/nickserv.c
@@ -57,7 +57,7 @@ class NickServCollide : public Timer
 	{
 		/* If they identified or don't exist anymore, don't kill them. */
 		User *u = finduser(na->nick);
-		if (!u || !nick_identified(u) || u->my_signon > this->GetSetTime())
+		if (!u || nick_identified(u) || u->my_signon > this->GetSetTime())
 			return;
 
 		/* The RELEASE timeout will always add to the beginning of the
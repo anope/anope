anope/anope

commit 0b3824c86a99354d06339b95e40515de762d65f7

Apply some changes based on possible "flaws" found with flawfinder.
git-svn-id: http://anope.svn.sourceforge.net/svnroot/anope/trunk@2574 5417fbe8-f217-4b02-8779-1006273d7864

src/core/enc_none.c
@@ -25,26 +25,25 @@ class ENone : public Module
 		ModuleManager::Attach(I_OnCheckPassword, this);
 	}
 
-	EventReturn OnEncrypt(const char *src,int len,char *dest,int size) 
+	EventReturn OnEncrypt(const char *src,int len,char *dest,int size)
 	{
-		if(size>=len) 
+		if(size>=len)
 		{
 			memset(dest,0,size);
-			strncpy(dest,src,len);
-			dest[len] = '\0';
-			return EVENT_ALLOW; 
+			strlcpy(dest,src,len);
+			return EVENT_ALLOW;
 		}
 		return EVENT_STOP;
 	}
 
-	EventReturn OnEncryptInPlace(char *buf, int size) 
+	EventReturn OnEncryptInPlace(char *buf, int size)
 	{
 		return EVENT_ALLOW;
 	}
 
-	EventReturn OnEncryptCheckLen(int passlen, int bufsize) 
+	EventReturn OnEncryptCheckLen(int passlen, int bufsize)
 	{
-		if(bufsize>=passlen) 
+		if(bufsize>=passlen)
 		{
 			return EVENT_ALLOW;
 		}
@@ -53,13 +52,12 @@ class ENone : public Module
 
 	EventReturn OnDecrypt(const char *src, char *dest, int size) {
 		memset(dest,0,size);
-		strncpy(dest,src,size);
-		dest[size] = '\0';
+		strlcpy(dest,src,size);
 		return EVENT_ALLOW;
 	}
 
 	EventReturn OnCheckPassword(const char *plaintext, const char *password) {
-		if(strcmp(plaintext,password)==0) 
+		if(strcmp(plaintext,password)==0)
 		{
 			return EVENT_ALLOW;
 		}

lang/langcomp.c
@@ -213,7 +213,7 @@ int main(int ac, char **av)
 					fprintf(stderr, "%s:%d: Out of memory!\n", filename, linenum);
 					return 2;
 				}
-				sprintf(strings[curstring] + i, "%s\n", line);
+				snprintf(strings[curstring] + i, strlen(line) + 2, "%s\n", line);
 			}
 		} else {
 			if ((curstring = stringnum(line)) < 0) {

src/channels.c
@@ -364,7 +364,7 @@ char *chan_get_modes(Channel * chan, int complete, int plus)
 						if (plus || !cmp->MinusNoArg)
 						{
 							chan->GetParam(cmp->Name, &param);
-							
+
 							if (!param.empty())
 							{
 								value = const_cast<char *>(param.c_str());
@@ -527,7 +527,7 @@ void chan_set_modes(const char *source, Channel *chan, int ac, const char **av, 
 					alog("debug: Setting %c%c on %s for %s", (add ? '+' : '-'),
 						 mode, chan->name, user->nick);
 
-				if (add) 
+				if (add)
 					chan_set_user_status(chan, user, cms->Status);
 					/* If this does +o, remove any DEOPPED flag */
 				else
@@ -565,7 +565,7 @@ void chan_set_modes(const char *source, Channel *chan, int ac, const char **av, 
 				if (cm->Type == MODE_PARAM)
 				{
 					cmp = static_cast<ChannelModeParam *>(cm);
-					
+
 					if (add || !cmp->MinusNoArg)
 					{
 						if (ac == 0)
@@ -1688,67 +1688,67 @@ void chan_set_correct_modes(User * user, Channel * c, int give_modes)
 	/* No need for strn* functions for modebuf, as every possible string
 	 * will always fit in. -GD
 	 */
-	strcpy(modebuf, "");
-	strcpy(userbuf, "");
+	strlcpy(modebuf, "", sizeof(modebuf));
+	strlcpy(userbuf, "", sizeof(userbuf));
 	if (add_modes > 0) {
 		strcat(modebuf, "+");
 		if ((add_modes & CUS_OWNER) && !(status & CUS_OWNER)) {
-			strcat(modebuf, &owner->ModeChar);
-			strcat(userbuf, " ");
-			strcat(userbuf, user->nick);
+			strlcat(modebuf, &owner->ModeChar, sizeof(modebuf));
+			strlcat(userbuf, " ", sizeof(userbuf));
+			strlcat(userbuf, user->nick, sizeof(userbuf));
 		} else {
 			add_modes &= ~CUS_OWNER;
 		}
 		if ((add_modes & CUS_PROTECT) && !(status & CUS_PROTECT)) {
-			strcat(modebuf, &admin->ModeChar);
-			strcat(userbuf, " ");
-			strcat(userbuf, user->nick);
+			strlcat(modebuf, &admin->ModeChar, sizeof(modebuf));
+			strlcat(userbuf, " ", sizeof(userbuf));
+			strlcat(userbuf, user->nick, sizeof(userbuf));
 		} else {
 			add_modes &= ~CUS_PROTECT;
 		}
 		if ((add_modes & CUS_OP) && !(status & CUS_OP)) {
-			strcat(modebuf, "o");
-			strcat(userbuf, " ");
-			strcat(userbuf, user->nick);
+			strlcat(modebuf, "o", sizeof(modebuf));
+			strlcat(userbuf, " ", sizeof(userbuf));
+			strlcat(userbuf, user->nick, sizeof(userbuf));
 		} else {
 			add_modes &= ~CUS_OP;
 		}
 		if ((add_modes & CUS_HALFOP) && !(status & CUS_HALFOP)) {
-			strcat(modebuf, "h");
-			strcat(userbuf, " ");
-			strcat(userbuf, user->nick);
+			strlcat(modebuf, "h", sizeof(modebuf));
+			strlcat(userbuf, " ", sizeof(userbuf));
+			strlcat(userbuf, user->nick, sizeof(userbuf));
 		} else {
 			add_modes &= ~CUS_HALFOP;
 		}
 		if ((add_modes & CUS_VOICE) && !(status & CUS_VOICE)) {
-			strcat(modebuf, "v");
-			strcat(userbuf, " ");
-			strcat(userbuf, user->nick);
+			strlcat(modebuf, "v", sizeof(modebuf));
+			strlcat(userbuf, " ", sizeof(userbuf));
+			strlcat(userbuf, user->nick, sizeof(userbuf));
 		} else {
 			add_modes &= ~CUS_VOICE;
 		}
 	}
 	if (rem_modes > 0) {
-		strcat(modebuf, "-");
+		strlcat(modebuf, "-", sizeof(modebuf));
 		if (rem_modes & CUS_OWNER) {
-			strcat(modebuf, &owner->ModeChar);
-			strcat(userbuf, " ");
-			strcat(userbuf, user->nick);
+			strlcat(modebuf, &owner->ModeChar, sizeof(modebuf));
+			strlcat(userbuf, " ", sizeof(userbuf));
+			strlcat(userbuf, user->nick, sizeof(userbuf));
 		}
 		if (rem_modes & CUS_PROTECT) {
-			strcat(modebuf, &admin->ModeChar);
-			strcat(userbuf, " ");
-			strcat(userbuf, user->nick);
+			strlcat(modebuf, &admin->ModeChar, sizeof(modebuf));
+			strlcat(userbuf, " ", sizeof(userbuf));
+			strlcat(userbuf, user->nick, sizeof(userbuf));
 		}
 		if (rem_modes & CUS_OP) {
-			strcat(modebuf, "o");
-			strcat(userbuf, " ");
-			strcat(userbuf, user->nick);
+			strlcat(modebuf, "o", sizeof(modebuf));
+			strlcat(userbuf, " ", sizeof(userbuf));
+			strlcat(userbuf, user->nick, sizeof(userbuf));
 		}
 		if (rem_modes & CUS_HALFOP) {
-			strcat(modebuf, "h");
-			strcat(userbuf, " ");
-			strcat(userbuf, user->nick);
+			strlcat(modebuf, "h", sizeof(modebuf));
+			strlcat(userbuf, " ", sizeof(userbuf));
+			strlcat(userbuf, user->nick, sizeof(userbuf));
 		}
 	}
 

src/chanserv.c
@@ -1227,7 +1227,7 @@ int check_kick(User * user, const char *chan, time_t chants)
 			if (akick->flags & AK_ISNICK)
 				get_idealban(ci, user, mask, sizeof(mask));
 			else
-				strcpy(mask, akick->u.mask);
+				strlcpy(mask, akick->u.mask, sizeof(mask));
 			reason = akick->reason ? akick->reason : CSAutokickReason;
 			goto kick;
 		}

src/core/cs_akick.c
@@ -213,10 +213,9 @@ class CommandCSAKick : public Command
 
 			if (!na) {
 				split_usermask(mask, &nick, &user, &host);
-				char *smask = new char[strlen(nick) + strlen(user) + strlen(host) + 3];
+				std::string smask = std::string(nick) + "!" + user + "@" + host;
 				freemask = 1;
-				sprintf(smask, "%s!%s@%s", nick, user, host);
-				mask = smask;
+				mask = sstrdup(smask.c_str());
 				delete [] nick;
 				delete [] user;
 				delete [] host;

src/core/ns_group.c
@@ -117,8 +117,8 @@ class CommandNSGroup : public Command
 
 			if (na)
 			{
-				na->last_usermask = new char[u->GetIdent().length() + u->GetDisplayedHost().length() + 2];
-				sprintf(na->last_usermask, "%s@%s", u->GetIdent().c_str(), u->GetDisplayedHost().c_str());
+				std::string last_usermask = u->GetIdent() + "@" + u->GetDisplayedHost();
+				na->last_usermask = sstrdup(last_usermask.c_str());
 				na->last_realname = sstrdup(u->realname);
 				na->time_registered = na->last_seen = time(NULL);
 

src/core/ns_register.c
@@ -49,8 +49,8 @@ class CommandNSConfirm : public Command
 		}
 		else
 		{
-			na->last_usermask = new char[u->GetIdent().length() + u->GetDisplayedHost().length() + 2];
-			sprintf(na->last_usermask, "%s@%s", u->GetIdent().c_str(), u->GetDisplayedHost().c_str());
+			std::string last_usermask = u->GetIdent() + "@" + u->GetDisplayedHost();
+			na->last_usermask = sstrdup(last_usermask.c_str());
 			na->last_realname = sstrdup(u->realname);
 			if (NSAddAccessOnReg)
 				na->nc->AddAccess(create_mask(u));

src/core/os_chankill.c
@@ -74,8 +74,8 @@ class CommandOSChanKill : public Command
 					cunext = cu->next;
 					if (is_oper(cu->user))
 						continue;
-					strncpy(mask, "*@", 3); /* Use *@" for the akill's, */
-					strncat(mask, cu->user->host, HOSTMAX);
+					strlcpy(mask, "*@", sizeof(mask)); /* Use *@" for the akill's, */
+					strlcat(mask, cu->user->host, sizeof(mask));
 					add_akill(NULL, mask, s_OperServ, expires, realreason);
 					check_akill(cu->user->nick, cu->user->GetIdent().c_str(), cu->user->host, NULL, NULL);
 				}

commit 9c6576f78e495a9b992c26d7da0b3c0c583e22c7

Fix bug introduced in r2574 that would set passwords incorrectly when using enc_none
git-svn-id: http://anope.svn.sourceforge.net/svnroot/anope/trunk@2646 5417fbe8-f217-4b02-8779-1006273d7864

src/core/enc_none.c
@@ -30,7 +30,7 @@ class ENone : public Module
 		if(size>=len)
 		{
 			memset(dest,0,size);
-			strlcpy(dest,src,len);
+			strlcpy(dest,src,len + 1);
 			return EVENT_ALLOW;
 		}
 		return EVENT_STOP;
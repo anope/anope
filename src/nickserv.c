anope/anope

commit b5242e3be0801abd0edebf62955b1d1b6cd3fd7d

Fixed bug #1106 - Anope now keeps track of users cloaked hosts as well as virtual host and will use both in matching for things
git-svn-id: http://anope.svn.sourceforge.net/svnroot/anope/stable@2679 5417fbe8-f217-4b02-8779-1006273d7864

src/nickserv.c
@@ -1281,8 +1281,7 @@ NickCore *findcore(const char *nick)
 int is_on_access(User * u, NickCore * nc)
 {
     int i;
-    char *buf;
-    char *buf2 = NULL;
+    char *buf, *buf2 = NULL, *buf3 = NULL;
 
     if (nc->accesscount == 0)
         return 0;
@@ -1294,24 +1293,33 @@ int is_on_access(User * u, NickCore * nc)
             buf2 = scalloc(strlen(u->username) + strlen(u->vhost) + 2, 1);
             sprintf(buf2, "%s@%s", u->username, u->vhost);
         }
+        if (u->chost)
+        {
+            buf3 = scalloc(strlen(u->username) + strlen(u->chost) + 2, 1);
+            sprintf(buf2, "%s@%s", u->username, u->chost);
+        }
     }
 
     for (i = 0; i < nc->accesscount; i++) {
-        if (match_wild_nocase(nc->access[i], buf)
-            || (ircd->vhost ? match_wild_nocase(nc->access[i], buf2) : 0)) {
+          if (match_wild_nocase(nc->access[i], buf) || (buf2 && match_wild_nocase(nc->access[i], buf2)) || (buf3 && match_wild_nocase(nc->access[i], buf3)))
+          {
             free(buf);
             if (ircd->vhost) {
                 if (u->vhost) {
                     free(buf2);
                 }
+                if (u->chost)
+                  free(buf3);
             }
             return 1;
         }
     }
     free(buf);
-    if (ircd->vhost) {
-        free(buf2);
-    }
+    if (buf2)
+    	free(buf2);
+    if (buf3)
+        free(buf3);
+    
     return 0;
 }
 

version.log
@@ -9,9 +9,10 @@ VERSION_MAJOR="1"
 VERSION_MINOR="8"
 VERSION_PATCH="2"
 VERSION_EXTRA="-svn"
-VERSION_BUILD="2671"
+VERSION_BUILD="2679"
 
 # $Log$ # Changes since 1.8.2 Release
+#Revision 2679 - Fixed bug #1106 - Anope now keeps track of users cloaked hosts as well as virtual host and will use both in matching for things
 #Revision 2671 - Fix bug #1114. Fixed a bug in cs_clear caused by do_cmode() modifying params passed to it. Also fixed some warnings in cs_xop.
 #Revision 2668 - Fix a few more XOP related problems.
 #Revision 2667 - Cleaned up a lot of the channel access reordering code, properly change users with less than voice access on channels to XOP, and fix a potential crashbug after switching to XOP on IRCds that do not support halfop

include/services.h
@@ -870,6 +870,7 @@ struct user_ {
     char *host;             	/* User's real hostname 	*/
     char *hostip;               /* User's IP number             */
     char *vhost;            	/* User's virtual hostname 	*/
+    char *chost;                /* User's cloaked hostname      */
     char *vident;           	/* User's virtual ident 	*/
     char *realname;		/* Realname 			*/
     Server *server;		/* Server user is connected to  */

src/actions.c
@@ -181,7 +181,8 @@ void common_unban(ChannelInfo * ci, char *nick)
         for (ban = ci->c->bans->entries; ban; ban = next) {
             next = ban->next;
             if (entry_match(ban, u->nick, u->username, u->host, ip) ||
-                entry_match(ban, u->nick, u->username, u->vhost, ip)) {
+                entry_match(ban, u->nick, u->username, u->vhost, ip) ||
+                entry_match(ban, u->nick, u->username, u->chost, ip)) {
                 anope_cmd_mode(whosends(ci), ci->name, "-b %s", ban->mask);
                 if (ircdcap->tsmode)
                     av[3] = ban->mask;
@@ -240,10 +241,10 @@ char *common_get_vhost(User * u)
     if (!u)
         return NULL;
 
-    if (ircd->vhostmode && (u->mode & ircd->vhostmode))
+    if (ircd->vhostmode && (u->mode & ircd->vhostmode) && u->vhost)
         return u->vhost;
-    else if (ircd->vhost && u->vhost)
-        return u->vhost;
+    else if (ircd->vhostmode && (u->mode & ircd->vhostmode) && u->chost)
+        return u->chost;
     else
         return u->host;
 }

src/core/hs_off.c
@@ -75,8 +75,12 @@ int do_off(User * u)
             vident = getvIdent(u->nick);
             if (vhost == NULL && vident == NULL)
                 notice_lang(s_HostServ, u, HOST_NOT_ASSIGNED);
-            else
+            else {
                 anope_cmd_vhost_off(u);
+                if (u->vhost)
+                   free(u->vhost);
+                u->vhost = NULL;
+            }
         } else {
             notice_lang(s_HostServ, u, HOST_ID);
         }

src/protocol/rageircd.c
@@ -668,7 +668,7 @@ void rageircd_cmd_topic(char *whosets, char *chan, char *whosetit,
 
 void rageircd_cmd_vhost_off(User * u)
 {
-    send_cmd(s_HostServ, "SVSMODE %s -x", u->nick);
+    common_svsmode(u, "-x", NULL);
     notice_lang(s_HostServ, u, HOST_OFF_UNREAL, u->nick, ircd->vhostchar);
 }
 

src/protocol/solidircd.c
@@ -1388,7 +1388,7 @@ void solidircd_cmd_vhost_on(char *nick, char *vIdent, char *vhost)
 
 void solidircd_cmd_vhost_off(User * u)
 {
-    send_cmd(s_HostServ, "SVSMODE %s -v", u->nick);
+    common_svsmode(u, "-v", NULL);
     notice_lang(s_HostServ, u, HOST_OFF_UNREAL, u->nick, ircd->vhostchar);
 }
 

src/protocol/ultimate3.c
@@ -720,7 +720,7 @@ void ultimate3_cmd_remove_akill(char *user, char *host)
 
 void ultimate3_cmd_vhost_off(User * u)
 {
-    send_cmd(s_HostServ, "SVSMODE %s -x", u->nick);
+    common_svsmode(u, "-x", NULL);
     notice_lang(s_HostServ, u, HOST_OFF_UNREAL, u->nick, ircd->vhostchar);
 }
 

commit a1c45be517908522df431d7f9dcc1dcb4a0baeec

Fix a crash introduced in r2679 cause by is_on_access messing up the users host buffers
git-svn-id: http://anope.svn.sourceforge.net/svnroot/anope/stable@2743 5417fbe8-f217-4b02-8779-1006273d7864

src/nickserv.c
@@ -1296,7 +1296,7 @@ int is_on_access(User * u, NickCore * nc)
         if (u->chost)
         {
             buf3 = scalloc(strlen(u->username) + strlen(u->chost) + 2, 1);
-            sprintf(buf2, "%s@%s", u->username, u->chost);
+            sprintf(buf3, "%s@%s", u->username, u->chost);
         }
     }
 

version.log
@@ -9,9 +9,10 @@ VERSION_MAJOR="1"
 VERSION_MINOR="8"
 VERSION_PATCH="2"
 VERSION_EXTRA="-svn"
-VERSION_BUILD="2735"
+VERSION_BUILD="2743"
 
 # $Log$ # Changes since 1.8.2 Release
+#Revision 2743 - Fix a crash introduced in r2679 cause by is_on_access messing up the users host buffers
 #Revision 2735 - Added two missing language strings to de.l and ru.l
 #Revision 2734 - Made elist_match_user also check against the users cloaked host
 #Revision 2731 - Mark users as unrecognized on Inspircd 1.2 if no/invalid metadata is recieved for them before the next uid/eob
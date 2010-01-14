anope/anope

commit 2708eea5d13c249143b83fbaa8b552992f0ac8b6

Removed NICKMAX and CHANMAX, replaced user->nick, c->name, and ci->name with std::string
git-svn-id: http://anope.svn.sourceforge.net/svnroot/anope/trunk@2732 5417fbe8-f217-4b02-8779-1006273d7864

include/services.h
@@ -495,7 +495,7 @@ class Memo : public Flags<MemoFlag>
  public:
 	uint32 number;	/* Index number -- not necessarily array position! */
 	time_t time;	/* When it was sent */
-	char sender[NICKMAX];
+	std::string sender;
 	char *text;
 };
 
@@ -854,11 +854,11 @@ class CoreExport Channel : public Extensible, public Flags<ChannelFlags>
 	~Channel();
 
 	Channel *next, *prev;
-	char name[CHANMAX];
+	std::string name;		/* Channel name */
 	ChannelInfo *ci;			/* Corresponding ChannelInfo */
 	time_t creation_time;		/* When channel was created */
 	char *topic;
-	char topic_setter[NICKMAX];		/* Who set the topic */
+	std::string topic_setter;
 	time_t topic_time;			/* When topic was set */
 	std::bitset<128> modes;
 
@@ -1055,7 +1055,7 @@ struct NewsItem
 	NewsType type;
 	uint32 num;
 	std::string Text;
-	char who[NICKMAX];
+	std::string who;
 	time_t time;
 };
 
@@ -1101,7 +1101,7 @@ struct sxline_ {
 struct exception_ {
 	char *mask;				 /* Hosts to which this exception applies */
 	int limit;				  /* Session limit for exception */
-	char who[NICKMAX];		  /* Nick of person who added the exception */
+	std::string who;		/* Nick of person who added the exception */
 	char *reason;			   /* Reason for exception's addition */
 	time_t time;				/* When this exception was added */
 	time_t expires;			 /* Time when it expires. 0 == no expiry */

src/core/db_plain.cpp
@@ -456,7 +456,7 @@ static void LoadChanInfo(const std::vector<std::string> &params)
 	ci->memos.memomax = atoi(params[4].c_str());
 
 	if (debug > 1)
-		alog("[db_plain]: loaded channel %s", ci->name);
+		alog("[db_plain]: loaded channel %s", ci->name.c_str());
 }
 
 static void LoadOperInfo(const std::vector<std::string> &params)
@@ -486,7 +486,7 @@ static void LoadOperInfo(const std::vector<std::string> &params)
 		exceptions = static_cast<Exception *>(srealloc(exceptions, sizeof(Exception) * nexceptions));
 		exceptions[nexceptions - 1].mask = sstrdup(params[1].c_str());
 		exceptions[nexceptions - 1].limit = atol(params[2].c_str());
-		strscpy(exceptions[nexceptions - 1].who, params[3].c_str(), NICKMAX);
+		exceptions[nexceptions - 1].who = params[3];
 		exceptions[nexceptions - 1].time = strtol(params[4].c_str(), NULL, 10);
 		exceptions[nexceptions - 1].expires = strtol(params[5].c_str(), NULL, 10);
 		exceptions[nexceptions - 1].reason = sstrdup(params[6].c_str());
@@ -572,7 +572,7 @@ class DBPlain : public Module
 			Memo *m = new Memo;
 			m->number = atoi(params[0].c_str());
 			m->time = strtol(params[1].c_str(), NULL, 10);
-			strscpy(m->sender, params[2].c_str(), NICKMAX);
+			m->sender = params[2];
 			unsigned j;
 			for (j = 3; (params[j] == "UNREAD" || params[j] == "RECEIPT" || params[j] == "NOTIFYS"); ++j)
 			{
@@ -656,7 +656,7 @@ class DBPlain : public Module
 			ci->founder = findcore(params[0].c_str());
 			if (!ci->founder)
 			{
-				alog("[db_plain]: Deleting founderless channel %s", ci->name);
+				alog("[db_plain]: Deleting founderless channel %s", ci->name.c_str());
 				delete ci;
 				ci = NULL;
 				throw "no founder";
@@ -686,7 +686,7 @@ class DBPlain : public Module
 			ci->email = sstrdup(params[0].c_str());
 		else if (key == "TOPIC")
 		{
-			strscpy(ci->last_topic_setter, params[0].c_str(), NICKMAX);
+			ci->last_topic_setter = params[0];
 			ci->last_topic_time = strtol(params[1].c_str(), NULL, 10);
 			ci->last_topic = sstrdup(params[2].c_str());
 		}
@@ -749,7 +749,7 @@ class DBPlain : public Module
 			Memo *m = new Memo;
 			m->number = atoi(params[0].c_str());
 			m->time = strtol(params[1].c_str(), NULL, 10);
-			strscpy(m->sender, params[2].c_str(), NICKMAX);
+			m->sender = params[2];
 			unsigned j;
 			for (j = 3; j < params.size() - 1 && (buf == "UNREAD" || buf == "RECEIPT" || buf == "NOTIFYS"); ++j)
 			{

src/core/os_session.c
@@ -167,7 +167,7 @@ static int exception_view(User *u, const int index, int *sent_header)
 
 	expire_left(u->nc, expirebuf, sizeof(expirebuf), exceptions[index].expires);
 
-	notice_lang(Config.s_OperServ, u, OPER_EXCEPTION_VIEW_FORMAT, index + 1, exceptions[index].mask, *exceptions[index].who ? exceptions[index].who : "<unknown>", timebuf, expirebuf, exceptions[index].limit, exceptions[index].reason);
+	notice_lang(Config.s_OperServ, u, OPER_EXCEPTION_VIEW_FORMAT, index + 1, exceptions[index].mask, !exceptions[index].who.empty() ? exceptions[index].who.c_str() : "<unknown>", timebuf, expirebuf, exceptions[index].limit, exceptions[index].reason);
 	return 1;
 }
 
@@ -243,7 +243,7 @@ class CommandOSException : public Command
 				return MOD_CONT;
 			}
 
-			x = exception_add(u, mask, limit, reason, u->nick, expires);
+			x = exception_add(u, mask, limit, reason, u->nick.c_str(), expires);
 
 			if (x == 1)
 				notice_lang(Config.s_OperServ, u, OPER_EXCEPTION_ADDED, mask, limit);

src/sessions.c
@@ -341,7 +341,7 @@ int exception_add(User * u, const char *mask, const int limit,
 	exceptions[nexceptions - 1].limit = limit;
 	exceptions[nexceptions - 1].reason = sstrdup(reason);
 	exceptions[nexceptions - 1].time = time(NULL);
-	strscpy(exceptions[nexceptions - 1].who, who, NICKMAX);
+	exceptions[nexceptions - 1].who = who;
 	exceptions[nexceptions - 1].expires = expires;
 	exceptions[nexceptions - 1].num = nexceptions - 1;
 

commit fadc61f89e3c914424fdd37333ff356370d54d24

Fixed crash caused by r2732 caused by adding someone to the exception list
git-svn-id: http://anope.svn.sourceforge.net/svnroot/anope/trunk@2756 5417fbe8-f217-4b02-8779-1006273d7864

include/services.h
@@ -1129,7 +1129,7 @@ struct sxline_ {
 struct exception_ {
 	char *mask;				 /* Hosts to which this exception applies */
 	int limit;				  /* Session limit for exception */
-	std::string who;		/* Nick of person who added the exception */
+	char *who;		/* Nick of person who added the exception */
 	char *reason;			   /* Reason for exception's addition */
 	time_t time;				/* When this exception was added */
 	time_t expires;			 /* Time when it expires. 0 == no expiry */

src/core/db_plain.cpp
@@ -481,7 +481,7 @@ static void LoadOperInfo(const std::vector<std::string> &params)
 		exceptions = static_cast<Exception *>(srealloc(exceptions, sizeof(Exception) * nexceptions));
 		exceptions[nexceptions - 1].mask = sstrdup(params[1].c_str());
 		exceptions[nexceptions - 1].limit = atol(params[2].c_str());
-		exceptions[nexceptions - 1].who = params[3];
+		exceptions[nexceptions - 1].who = sstrdup(params[3].c_str());
 		exceptions[nexceptions - 1].time = strtol(params[4].c_str(), NULL, 10);
 		exceptions[nexceptions - 1].expires = strtol(params[5].c_str(), NULL, 10);
 		exceptions[nexceptions - 1].reason = sstrdup(params[6].c_str());

src/core/os_session.c
@@ -167,7 +167,7 @@ static int exception_view(User *u, const int index, int *sent_header)
 
 	expire_left(u->nc, expirebuf, sizeof(expirebuf), exceptions[index].expires);
 
-	notice_lang(Config.s_OperServ, u, OPER_EXCEPTION_VIEW_FORMAT, index + 1, exceptions[index].mask, !exceptions[index].who.empty() ? exceptions[index].who.c_str() : "<unknown>", timebuf, expirebuf, exceptions[index].limit, exceptions[index].reason);
+	notice_lang(Config.s_OperServ, u, OPER_EXCEPTION_VIEW_FORMAT, index + 1, exceptions[index].mask, exceptions[index].who ? exceptions[index].who : "<unknown>", timebuf, expirebuf, exceptions[index].limit, exceptions[index].reason);
 	return 1;
 }
 

src/sessions.c
@@ -266,6 +266,7 @@ void expire_exceptions()
 							 exceptions[i].mask);
 		delete [] exceptions[i].mask;
 		delete [] exceptions[i].reason;
+		delete [] exceptions[i].who;
 		nexceptions--;
 		memmove(exceptions + i, exceptions + i + 1,
 				sizeof(Exception) * (nexceptions - i));
@@ -335,13 +336,13 @@ int exception_add(User * u, const char *mask, const int limit,
 	}
 
 	nexceptions++;
-	exceptions = static_cast<Exception *>(srealloc(exceptions, sizeof(Exception) * nexceptions));
+	exceptions = new Exception[nexceptions];
 
 	exceptions[nexceptions - 1].mask = sstrdup(mask);
 	exceptions[nexceptions - 1].limit = limit;
 	exceptions[nexceptions - 1].reason = sstrdup(reason);
 	exceptions[nexceptions - 1].time = time(NULL);
-	exceptions[nexceptions - 1].who = who;
+	exceptions[nexceptions - 1].who = sstrdup(who);
 	exceptions[nexceptions - 1].expires = expires;
 	exceptions[nexceptions - 1].num = nexceptions - 1;
 
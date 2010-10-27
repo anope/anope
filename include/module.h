a79da4bba11ef6fec0ef010a7900c742c2e358ad
Adam <Adam@anope.org>
2010-10-26 22:02:08 -0400

    Modules to not need to include libintl.h because they are not linked to libintl and do not directly call its code. #define _(x) to x just so xgettext is able to pick out language strings.

--- include/module.h
+++ include/module.h
@@ -4,12 +4,6 @@
 #include "services.h"
 #include "commands.h"
 #include "modules.h"
-
-#if GETTEXT_FOUND
-# include <libintl.h>
-# define _(x) gettext(x)
-#else
-# define _(x) x
-#endif
+#define _(x) x
 
 #endif // MODULE_H

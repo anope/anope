d896bf9a1cb1d875fa6930380fc001c76fc44b48
Adam <Adam@anope.org>
2010-12-27 01:00:12 -0500

    Bug #1222 - Fixed crash from reading to read a range of memos

--- modules/core/ms_read.cpp
+++ modules/core/ms_read.cpp
@@ -49,7 +49,7 @@ class MemoListCallback : public NumberList
 class CommandMSRead : public Command
 {
  public:
-	CommandMSRead() : Command("READ", 0, 2)
+	CommandMSRead() : Command("READ", 1, 2)
 	{
 	}
 
@@ -59,8 +59,7 @@ class CommandMSRead : public Command
 
 		MemoInfo *mi;
 		ChannelInfo *ci = NULL;
-		Anope::string numstr = !params.empty() ? params[0] : "", chan;
-		int num;
+		Anope::string numstr = params[0], chan;
 
 		if (!numstr.empty() && numstr[0] == '#')
 		{
@@ -81,8 +80,8 @@ class CommandMSRead : public Command
 		}
 		else
 			mi = &u->Account()->memos;
-		num = !numstr.empty() && numstr.is_number_only() ? convertTo<int>(numstr) : -1;
-		if (numstr.empty() || (!numstr.equals_ci("LAST") && !numstr.equals_ci("NEW") && num <= 0))
+
+		if (numstr.empty() || (!numstr.equals_ci("LAST") && !numstr.equals_ci("NEW") && !numstr.is_number_only()))
 			this->OnSyntaxError(source, numstr);
 		else if (mi->memos.empty())
 		{
@@ -91,7 +90,8 @@ class CommandMSRead : public Command
 			else
 				source.Reply(MEMO_HAVE_NO_MEMOS);
 		}
-		else {
+		else
+		{
 			int i, end;
 
 			if (numstr.equals_ci("NEW"))

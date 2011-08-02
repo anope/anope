41b40f6504b33e720233d5bcbdd5fce998d7d86f
Adam <Adam@anope.org>
2011-08-01 23:42:20 -0400

    Split /os mode into /os mode and /os umode to make giving permission to just one possible

--- modules/core/os_mode.cpp
+++ modules/core/os_mode.cpp
@@ -18,8 +18,8 @@ class CommandOSMode : public Command
  public:
 	CommandOSMode(Module *creator) : Command(creator, "operserv/mode", 2, 2, "operserv/mode")
 	{
-		this->SetDesc(_("Change channel or user modes"));
-		this->SetSyntax(_("{\037channel\037|\037user\037} \037modes\037"));
+		this->SetDesc(_("Change channel modes"));
+		this->SetSyntax(_("\037channel\037 \037modes\037"));
 	}
 
 	void Execute(CommandSource &source, const std::vector<Anope::string> &params)
@@ -28,46 +28,64 @@ class CommandOSMode : public Command
 		const Anope::string &target = params[0];
 		const Anope::string &modes = params[1];
 
-		if (target[0] == '#')
-		{
-			Channel *c = findchan(target);
-			if (!c)
-				source.Reply(CHAN_X_NOT_IN_USE, target.c_str());
-			else if (c->bouncy_modes)
-				source.Reply(_("Services is unable to change modes. Are your servers' U:lines configured correctly?"));
-			else
-			{
-				c->SetModes(source.owner, false, modes.c_str());
-
-				Log(LOG_ADMIN, u, this) << modes << " on " << target;
-			}
-		}
+		Channel *c = findchan(target);
+		if (!c)
+			source.Reply(CHAN_X_NOT_IN_USE, target.c_str());
+		else if (c->bouncy_modes)
+			source.Reply(_("Services is unable to change modes. Are your servers' U:lines configured correctly?"));
 		else
 		{
-			User *u2 = finduser(target);
-			if (!u2)
-				source.Reply(NICK_X_NOT_IN_USE, target.c_str());
-			else
-			{
-				u2->SetModes(source.owner, "%s", modes.c_str());
-				source.Reply(_("Changed usermodes of \002%s\002 to %s."), u2->nick.c_str(), modes.c_str());
+			c->SetModes(source.owner, false, modes.c_str());
 
-				u2->SendMessage(source.owner, _("\002%s\002 changed your usermodes to %s."), u->nick.c_str(), modes.c_str());
-
-				Log(LOG_ADMIN, u, this) << modes << " on " << target;
-			}
+			Log(LOG_ADMIN, u, this) << modes << " on " << target;
 		}
-
-		return;
 	}
 
 	bool OnHelp(CommandSource &source, const Anope::string &subcommand)
 	{
 		this->SendSyntax(source);
 		source.Reply(" ");
-		source.Reply(_("Allows Services operators to change modes for any channel or\n"
-				"user. Parameters are the same as for the standard /MODE\n"
-				"command."));
+		source.Reply(_("Allows Services operators to change modes for any channel.\n"
+				"Parameters are the same as for the standard /MODE command."));
+		return true;
+	}
+};
+
+class CommandOSUMode : public Command
+{
+ public:
+	CommandOSUMode(Module *creator) : Command(creator, "operserv/umode", 2, 2, "operserv/umode")
+	{
+		this->SetDesc(_("Change channel or user modes"));
+		this->SetSyntax(_("\037user\037 \037modes\037"));
+	}
+
+	void Execute(CommandSource &source, const std::vector<Anope::string> &params)
+	{
+		User *u = source.u;
+		const Anope::string &target = params[0];
+		const Anope::string &modes = params[1];
+
+		User *u2 = finduser(target);
+		if (!u2)
+			source.Reply(NICK_X_NOT_IN_USE, target.c_str());
+		else
+		{
+			u2->SetModes(source.owner, "%s", modes.c_str());
+			source.Reply(_("Changed usermodes of \002%s\002 to %s."), u2->nick.c_str(), modes.c_str());
+
+			u2->SendMessage(source.owner, _("\002%s\002 changed your usermodes to %s."), u->nick.c_str(), modes.c_str());
+
+			Log(LOG_ADMIN, u, this) << modes << " on " << target;
+		}
+	}
+
+	bool OnHelp(CommandSource &source, const Anope::string &subcommand)
+	{
+		this->SendSyntax(source);
+		source.Reply(" ");
+		source.Reply(_("Allows Services operators to change modes for any user.\n"
+				"Parameters are the same as for the standard /MODE command."));
 		return true;
 	}
 };
@@ -75,14 +93,16 @@ class CommandOSMode : public Command
 class OSMode : public Module
 {
 	CommandOSMode commandosmode;
+	CommandOSUMode commandosumode;
 
  public:
 	OSMode(const Anope::string &modname, const Anope::string &creator) : Module(modname, creator, CORE),
-		commandosmode(this)
+		commandosmode(this), commandosumode(this)
 	{
 		this->SetAuthor("Anope");
 
 		ModuleManager::RegisterService(&commandosmode);
+		ModuleManager::RegisterService(&commandosumode);
 	}
 };
 

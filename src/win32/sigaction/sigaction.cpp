4bdc9824a00f2a93ff6d75fae97b386938f648dc
Adam <Adam@anope.org>
2011-08-10 01:34:14 -0400

    Added two missing files

---
+++ src/win32/sigaction/sigaction.cpp
@@ -0,0 +1,31 @@
+ /* POSIX emulation layer for Windows.
+ *
+ * Copyright (C) 2008-2011 Anope Team <team@anope.org>
+ *
+ * Please read COPYING and README for further details.
+ *
+ * Based on the original code of Epona by Lara.
+ * Based on the original code of Services by Andy Church.
+ */
+ 
+ #include <windows.h>
+ #include "sigaction.h"
+ #include <signal.h>
+ 
+ int sigaction(int sig, struct sigaction *action, struct sigaction *old)
+ {
+	if (sig == -1)
+		return 0;
+	if (old == NULL)
+	{
+		if (signal(sig, SIG_DFL) == SIG_ERR)
+			return -1;
+	}
+	else
+	{
+		if (signal(sig, action->sa_handler) == SIG_ERR)
+			return -1;
+	}
+	return 0;
+ }
+ 

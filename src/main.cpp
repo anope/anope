78b8b307663b523964d728b39c9c8d51a544417a
Adam <Adam@anope.org>
2011-08-14 15:27:20 -0400

    Fixed resolving hosts when connecting to our uplink

--- src/main.cpp
+++ src/main.cpp
@@ -191,7 +191,8 @@ static void Connect()
 	if (!Config->LocalHost.empty())
 		UplinkSock->Bind(Config->LocalHost);
 	FOREACH_MOD(I_OnPreServerConnect, OnPreServerConnect());
-	UplinkSock->Connect(u->host, u->port);
+	DNSRecord req = DNSManager::BlockingQuery(u->host, u->ipv6 ? DNS_QUERY_AAAA : DNS_QUERY_A);
+	UplinkSock->Connect(req.result, u->port);
 }
 
 /*************************************************************************/

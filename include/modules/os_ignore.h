80588fba818b746d43adf978d09933fda79b5e1f
Adam <Adam@anope.org>
2014-02-17 19:43:19 -0500

    Fix os_ignore to work correctly with SQL

--- include/modules/os_ignore.h
+++ include/modules/os_ignore.h
@@ -10,70 +10,36 @@
  */
 
 
-struct IgnoreData : Serializable
+struct IgnoreData
 {
 	Anope::string mask;
 	Anope::string creator;
 	Anope::string reason;
 	time_t time; /* When do we stop ignoring them? */
 
-	IgnoreData() : Serializable("IgnoreData") { }
-	void Serialize(Serialize::Data &data) const anope_override;
-	static Serializable* Unserialize(Serializable *obj, Serialize::Data &data);
+	virtual ~IgnoreData() { }
+ protected:
+	IgnoreData() : time(0) { }
 };
 
 class IgnoreService : public Service
 {
  protected:
-	std::list<IgnoreData> ignores;
-
 	IgnoreService(Module *c) : Service(c, "IgnoreService", "ignore") { }
 	
  public:
-	virtual IgnoreData* AddIgnore(const Anope::string &mask, const Anope::string &creator, const Anope::string &reason, time_t delta = Anope::CurTime) = 0;
+	virtual void AddIgnore(IgnoreData *) = 0;
 
-	virtual bool DelIgnore(const Anope::string &mask) = 0;
+	virtual void DelIgnore(IgnoreData *) = 0;
 
-	inline void ClearIgnores() { this->ignores.clear(); }
+	virtual void ClearIgnores() = 0;
+
+	virtual IgnoreData *Create() = 0;
 
 	virtual IgnoreData *Find(const Anope::string &mask) = 0;
 
-	inline std::list<IgnoreData> &GetIgnores() { return this->ignores; }
+	virtual std::vector<IgnoreData *> &GetIgnores() = 0;
 };
 
 static ServiceReference<IgnoreService> ignore_service("IgnoreService", "ignore");
-
-void IgnoreData::Serialize(Serialize::Data &data) const
-{
-	data["mask"] << this->mask;
-	data["creator"] << this->creator;
-	data["reason"] << this->reason;
-	data["time"] << this->time;
-}
-
-Serializable* IgnoreData::Unserialize(Serializable *obj, Serialize::Data &data)
-{
-	if (!ignore_service)
-		return NULL;
-	
-	if (obj)
-	{
-		IgnoreData *ign = anope_dynamic_static_cast<IgnoreData *>(obj);
-		data["mask"] >> ign->mask;
-		data["creator"] >> ign->creator;
-		data["reason"] >> ign->reason;
-		data["time"] >> ign->time;
-		return ign;
-	}
-
-	Anope::string smask, screator, sreason;
-	time_t t;
-
-	data["mask"] >> smask;
-	data["creator"] >> screator;
-	data["reason"] >> sreason;
-	data["time"] >> t;
-
-	return ignore_service->AddIgnore(smask, screator, sreason, t);
-}
 

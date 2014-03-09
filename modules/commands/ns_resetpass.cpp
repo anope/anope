1a1a781b72d7920be44ffdbf582057279fe8125e
Adam <Adam@anope.org>
2014-03-09 08:34:34 -0400

    Remove mail:restrict as it is no longer useful. Changes nickserv/resetpass default to allow normal users to use it.

--- modules/commands/ns_resetpass.cpp
+++ modules/commands/ns_resetpass.cpp
@@ -27,9 +27,7 @@ class CommandNSResetPass : public Command
 	{
 		const NickAlias *na;
 
-		if (Config->GetBlock("mail")->Get<bool>("restrict") && !source.HasCommand("nickserv/resetpass"))
-			source.Reply(ACCESS_DENIED);
-		else if (!(na = NickAlias::Find(params[0])))
+		if (!(na = NickAlias::Find(params[0])))
 			source.Reply(NICK_X_NOT_REGISTERED, params[0].c_str());
 		else if (!na->nc->email.equals_ci(params[1]))
 			source.Reply(_("Incorrect email address."));

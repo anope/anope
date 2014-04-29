anope/anope

commit 82008d507cccbf24c73502bc97bc492d21305549

Fix Stas::Unserialize to return the instance

modules/commands/os_stats.cpp
@@ -14,7 +14,12 @@
 
 struct Stats : Serializable
 {
-	Stats() : Serializable("Stats") { }
+	static Stats *me;
+
+	Stats() : Serializable("Stats")
+	{
+		me = this;
+	}
 
 	void Serialize(Serialize::Data &data) const anope_override
 	{
@@ -26,7 +31,7 @@ struct Stats : Serializable
 	{
 		data["maxusercnt"] >> MaxUserCount;
 		data["maxusertime"] >> MaxUserTime;
-		return NULL;
+		return me;
 	}
 };
 

commit 3049916ade0c31c2edf2fca98456bccc14d06270

Fix undefined symbol error when loading os_stats, caused by 82008d507cccbf24c73502bc97bc492d21305549

modules/commands/os_stats.cpp
@@ -35,6 +35,8 @@ struct Stats : Serializable
 	}
 };
 
+Stats *Stats::me;
+
 /**
  * Count servers connected to server s
  * @param s The server to start counting from
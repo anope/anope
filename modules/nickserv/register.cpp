d68778166ace1af358f1e2391454a9d3f1aafbae
Adam <Adam@anope.org>
2017-04-20 15:37:52 -0400

    ns_register: notify users of unconfirmed accounts on register when there is no user too

--- modules/nickserv/register.cpp
+++ modules/nickserv/register.cpp
@@ -265,7 +265,6 @@ class CommandNSRegister : public Command
 		if (nsregister.equals_ci("admin"))
 		{
 			nc->SetUnconfirmed(true);
-			// User::Identify() called below will notify the user that their registration is pending
 		}
 		else if (nsregister.equals_ci("mail"))
 		{
@@ -280,8 +279,16 @@ class CommandNSRegister : public Command
 
 		if (u)
 		{
+			// This notifies the user that their registration is unconfirmed
 			u->Identify(na);
 			u->lastnickreg = Anope::CurTime;
+		}
+		else if (nc->IsUnconfirmed())
+		{
+			if (nsregister.equals_ci("admin"))
+				source.Reply(_("All new accounts must be validated by an administrator. Please wait for your registration to be confirmed."));
+			else if (nsregister.equals_ci("mail"))
+				source.Reply(_("Your email address is not confirmed. To confirm it, follow the instructions that were emailed to you."));
 		}
 	}
 

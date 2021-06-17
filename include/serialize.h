83e55efe111ada0bdeba14f685301dfde0cfc810
Josh Soref <jsoref@users.noreply.github.com>
2021-06-17 02:21:01 -0400

    spelling: serializable
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- include/serialize.h
+++ include/serialize.h
@@ -46,7 +46,7 @@ namespace Serialize
 	template<typename T> class Reference;
 }
 
-/** A serialziable object. Serializable objects can be serialized into
+/** A serializable object. Serializable objects can be serialized into
  * abstract data types (Serialize::Data), and then reconstructed or
  * updated later at any time.
  */
@@ -105,7 +105,7 @@ class CoreExport Serializable : public virtual Base
 };
 
 /* A serializable type. There should be one of these classes for each type
- * of class that inherits from Serialiable. Used for unserializing objects
+ * of class that inherits from Serializable. Used for unserializing objects
  * of this type, as it requires a function pointer to a static member function.
  */
 class CoreExport Serialize::Type : public Base

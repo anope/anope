73dcfb53139ee5d287939b542abe16e90af9dfda
Austin Ellis <siniStar@IRC4Fun.net>
2022-04-24 23:31:13 -0500

    webcpanel: add NickServ/confirm
    Add the ability to confirm accounts via webcpanel after logging in.

---
+++ modules/webcpanel/pages/nickserv/confirm.h
@@ -0,0 +1,24 @@
+/*
+ * (C) 2003-2022 Anope Team
+ * Contact us at team@anope.org
+ *
+ * Please read COPYING and README for further details.
+ */
+
+namespace WebCPanel
+{
+
+namespace NickServ
+{
+
+class Confirm : public WebPanelProtectedPage
+{
+ public:
+	Confirm(const Anope::string &cat, const Anope::string &u);
+
+	bool OnRequest(HTTPProvider *, const Anope::string &, HTTPClient *, HTTPMessage &, HTTPReply &, NickAlias *, TemplateFileServer::Replacements &) anope_override;
+};
+
+}
+
+}

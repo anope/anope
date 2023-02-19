a5d4aa582195a3037be1bc5c05fcd1067980676a
Sadie Powell <sadie@witchery.services>
2023-02-19 11:05:49 +0000

    Remove line numbers from the translation files.
    The translations are updated so infrequently that these always
    change a bunch and it makes reading the diffs really hard.

--- language/update.sh
+++ language/update.sh
@@ -4,13 +4,14 @@ rm -f anope.pot
 touch anope.pot
 
 cd ..
-FILES=`find ./ -name *.cpp -o -name *.h -o -name *.conf | grep -v /modules/third/`
+FILES=`find ./ -name *.cpp -o -name *.h -o -name *.conf | grep -v /docs/	 | grep -v /modules/third/`
 xgettext -E -C -s -d Anope -j -o language/anope.pot --from-code=utf-8 --keyword --keyword=_ $FILES
 cd -
 
 for f in *.po
 do
-	msgmerge -v -s -U $f `echo $f | cut -d'.' -f1`.pot
+	echo "Merging $f"
+	msgmerge --no-location --no-wrap --sort-output --update --verbose $f `echo $f | cut -d'.' -f1`.pot
 done
 
 rm -f *~

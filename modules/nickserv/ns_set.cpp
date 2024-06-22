8c0edef714a358cbc3fcc57dc02745bea6f76869
Sadie Powell <sadie@witchery.services>
2024-06-22 17:46:03 +0100

    Split nickserv/{sa,}set/language out to its own module.

--- modules/nickserv/ns_set.cpp
+++ modules/nickserv/ns_set.cpp
@@ -892,120 +892,6 @@ public:
 	}
 };
 
-class CommandNSSetLanguage
-	: public Command
-{
-public:
-	CommandNSSetLanguage(Module *creator, const Anope::string &sname = "nickserv/set/language", size_t min = 1) : Command(creator, sname, min, min + 1)
-	{
-		this->SetDesc(_("Set the language services will use when messaging you"));
-		this->SetSyntax(_("\037language\037"));
-	}
-
-	void Run(CommandSource &source, const Anope::string &user, const Anope::string &param)
-	{
-		if (Anope::ReadOnly)
-		{
-			source.Reply(READ_ONLY_MODE);
-			return;
-		}
-
-		const NickAlias *na = NickAlias::Find(user);
-		if (!na)
-		{
-			source.Reply(NICK_X_NOT_REGISTERED, user.c_str());
-			return;
-		}
-		NickCore *nc = na->nc;
-
-		EventReturn MOD_RESULT;
-		FOREACH_RESULT(OnSetNickOption, MOD_RESULT, (source, this, nc, param));
-		if (MOD_RESULT == EVENT_STOP)
-			return;
-
-		if (param != "en_US")
-			for (unsigned j = 0; j < Language::Languages.size(); ++j)
-			{
-				if (Language::Languages[j] == param)
-					break;
-				else if (j + 1 == Language::Languages.size())
-				{
-					this->OnSyntaxError(source, "");
-					return;
-				}
-			}
-
-		Log(nc == source.GetAccount() ? LOG_COMMAND : LOG_ADMIN, source, this) << "to change the language of " << nc->display << " to " << param;
-
-		nc->language = param;
-		if (source.GetAccount() == nc)
-			source.Reply(_("Language changed to \002English\002."));
-		else
-			source.Reply(_("Language for \002%s\002 changed to \002%s\002."), nc->display.c_str(), Language::Translate(param.c_str(), _("English")));
-	}
-
-	void Execute(CommandSource &source, const std::vector<Anope::string> &param) override
-	{
-		this->Run(source, source.nc->display, param[0]);
-	}
-
-	bool OnHelp(CommandSource &source, const Anope::string &) override
-	{
-		this->SendSyntax(source);
-		source.Reply(" ");
-		source.Reply(_("Changes the language services uses when sending messages to\n"
-				"you (for example, when responding to a command you send).\n"
-				"\037language\037 should be chosen from the following list of\n"
-				"supported languages:"));
-
-		source.Reply("         en_US (English)");
-		for (const auto &language : Language::Languages)
-		{
-			const Anope::string &langname = Language::Translate(language.c_str(), _("English"));
-			if (langname == "English")
-				continue;
-			source.Reply("         %s (%s)", language.c_str(), langname.c_str());
-		}
-
-		return true;
-	}
-};
-
-class CommandNSSASetLanguage final
-	: public CommandNSSetLanguage
-{
-public:
-	CommandNSSASetLanguage(Module *creator) : CommandNSSetLanguage(creator, "nickserv/saset/language", 2)
-	{
-		this->ClearSyntax();
-		this->SetSyntax(_("\037nickname\037 \037language\037"));
-	}
-
-	void Execute(CommandSource &source, const std::vector<Anope::string> &params) override
-	{
-		this->Run(source, params[0], params[1]);
-	}
-
-	bool OnHelp(CommandSource &source, const Anope::string &) override
-	{
-		this->SendSyntax(source);
-		source.Reply(" ");
-		source.Reply(_("Changes the language services uses when sending messages to\n"
-				"the given user (for example, when responding to a command they send).\n"
-				"\037language\037 should be chosen from the following list of\n"
-				"supported languages:"));
-		source.Reply("         en (English)");
-		for (const auto &language : Language::Languages)
-		{
-			const Anope::string &langname = Language::Translate(language.c_str(), _("English"));
-			if (langname == "English")
-				continue;
-			source.Reply("         %s (%s)", language.c_str(), langname.c_str());
-		}
-		return true;
-	}
-};
-
 class CommandNSSetMessage
 	: public Command
 {
@@ -1189,9 +1075,6 @@ class NSSet final
 	CommandNSSetKill commandnssetkill;
 	CommandNSSASetKill commandnssasetkill;
 
-	CommandNSSetLanguage commandnssetlanguage;
-	CommandNSSASetLanguage commandnssasetlanguage;
-
 	CommandNSSetMessage commandnssetmessage;
 	CommandNSSASetMessage commandnssasetmessage;
 
@@ -1262,7 +1145,6 @@ public:
 		commandnssetemail(this), commandnssasetemail(this),
 		commandnssetkeepmodes(this), commandnssasetkeepmodes(this),
 		commandnssetkill(this), commandnssasetkill(this),
-		commandnssetlanguage(this), commandnssasetlanguage(this),
 		commandnssetmessage(this), commandnssasetmessage(this),
 		commandnssetpassword(this), commandnssasetpassword(this),
 		commandnssasetnoexpire(this),

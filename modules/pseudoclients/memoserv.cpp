6da4a148fa67b86c8c067a50bfa16390b0b24687
Sadie Powell <sadie@witchery.services>
2024-08-24 12:27:05 +0100

    Fix translating mail:memo_message.

--- modules/pseudoclients/memoserv.cpp
+++ modules/pseudoclients/memoserv.cpp
@@ -18,7 +18,7 @@ class MemoServCore : public Module, public MemoServService
 	bool SendMemoMail(NickCore *nc, MemoInfo *mi, Memo *m)
 	{
 		Anope::string subject = Language::Translate(nc, Config->GetBlock("mail")->Get<const Anope::string>("memo_subject").c_str()),
-			message = Language::Translate(Config->GetBlock("mail")->Get<const Anope::string>("memo_message").c_str());
+			message = Language::Translate(nc, Config->GetBlock("mail")->Get<const Anope::string>("memo_message").c_str());
 
 		subject = subject.replace_all_cs("%n", nc->display);
 		subject = subject.replace_all_cs("%s", m->sender);
